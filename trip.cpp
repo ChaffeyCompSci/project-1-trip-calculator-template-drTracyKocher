16e0dd53a036d50191cb15a0a3fad74b0a83bdec
drTracyKocher <123515480+drTracyKocher@users.noreply.github.com>
2026-03-06 15:11:46 -0800

    Fix gas cost and total trip cost calculations

--- trip.cpp
+++ trip.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 double gas_cost(double miles, double mpg, double gas_price) {
     // TODO: calculate gas cost
+    double gas = miles * mpg * gas_price
 }
 
 double total_trip_cost(double gas, double food, double hotel) {
     // TODO: calculate total cost of gas food and hotel
+    double totalCost = gas + food + hotel
 }
 
 int main() {
     cout << "Trip Cost Calculator" << endl;
+    
 }
