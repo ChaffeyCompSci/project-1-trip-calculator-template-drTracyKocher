a658d5a1556acad8c147597658e598775623afa0
drTracyKocher <123515480+drTracyKocher@users.noreply.github.com>
2026-03-06 15:19:53 -0800

    Add main function to calculate trip costs

--- trip.cpp
+++ trip.cpp
@@ -10,3 +10,13 @@ double total_trip_cost(double gas, double food, double hotel) {
     double totalCost = gas + food + hotel;
     return totalCost;
 }
+
+int main() {
+    double gas = gas_cost(100, 25, 4.0);
+    double total = total_trip_cost(gas, 20, 50);
+
+    cout << "Gas cost: " << gas << endl;
+    cout << "Total trip cost: " << total << endl;
+
+    return 0;
+}
